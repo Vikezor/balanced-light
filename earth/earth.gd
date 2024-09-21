be70e5fc3500f6e29d4da52f94065139bb4a689e
Viktor Mitts <viktor.mitts@aalto.fi>
2024-09-21 20:07:32 +0300

    add a variable for the sun function

--- earth/earth.gd
+++ earth/earth.gd
@@ -4,6 +4,9 @@ extends Node2D
 var speed: float = -2 * PI / (24 * 60 * 60)
 @export var thrust: float = 0
 var orbital_angular_velocity: float = -2 * PI / (365.256363004 * 24 * 60 * 60)
+@export var sun_function: Callable = func (time):
+	return time * orbital_angular_velocity
+var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +17,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	speed += thrust * delta
 	$Diffuse.rotation += speed * delta
-	$Shading.rotation += orbital_angular_velocity * delta
+	$Shading.rotation = sun_function.call(time)
+	time += delta
