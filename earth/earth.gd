29b0889c7cb68882dc94adaccc4392752f0c2712
bob <bachpq2003@gmail.com>
2024-09-21 21:28:42 +0300

    Sun orbit weird

--- earth/earth.gd
+++ earth/earth.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 var speed: float = -2 * PI / (24 * 60 * 60)
 @export var thrust: float = 0
+@export var thrust_strength = 3
+@export var max_turning_speed = 6
 var time = 0
 
 
@@ -14,7 +16,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	speed += thrust * delta
+	var thrust_control = Input.get_action_strength("right_thrust") - Input.get_action_strength("left_thrust")
+	speed += thrust_control * thrust_strength * delta
+	speed = clamp(speed, -max_turning_speed, max_turning_speed)
 	$Diffuse.rotation += speed * delta
 	time += delta
 
--- view/sun.gd
+++ view/sun.gd
@@ -5,9 +5,29 @@ var time = 0
 
 signal set_sun_rotation
 
+var time_per_function:float  = 20
+
 var orbital_angular_velocity: float = -2 * PI / (365.256363004 * 24 * 60 * 60)
-@export var sun_function: Callable = func (t):
-	return t * orbital_angular_velocity
+
+var sun_function_1: Callable = func (t):
+	return t
+
+var sun_function_2: Callable = func (t):
+	return t + pow(sin(t * PI / 3), 2)
+	
+var sun_function_3: Callable = func(t):
+	return pow(sin(t * PI / 2.5) + 1, 2) 
+	
+var sun_function_4: Callable = func(t):
+	return log(sin(t * PI / 3) + 1.1)
+
+var sun_function_5: Callable = func(t):
+	return sin(t * PI / 3) + cos(t * PI)
+	
+var sun_function_6: Callable = func(t):
+	return sin(t * PI / 3) + cos(1 + t * PI * 2 / 3) + sin(2 + t * PI * PI) + sin(-1 + t * PI * 4 / 3)
+
+var sun_functions = [sun_function_1, sun_function_2, sun_function_3, sun_function_4, sun_function_5, sun_function_6]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,5 +43,5 @@ func _process(delta: float) -> void:
 	time += delta
 	
 
-func get_sun_rotation() -> float:
-	return sun_function.call(time)
+func get_sun_rotation(time_from_current:float = 0) -> float:
+	return fmod(sun_functions[5].call(time + time_from_current)*PI/3, PI * 2)
